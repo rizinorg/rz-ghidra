7acc931790feafe29600a893f3c3c1abb7cfb480
Itay Cohen <itaycohen23@gmail.com>
2019-08-29 14:57:29 +0300

    Make the cutter plugin async (#23)

--- cutter-plugin/R2GhidraDecompiler.cpp
+++ cutter-plugin/R2GhidraDecompiler.cpp
@@ -9,43 +9,52 @@
 R2GhidraDecompiler::R2GhidraDecompiler(QObject *parent)
 	: Decompiler("r2ghidra", "Ghidra", parent)
 {
+	task = nullptr;
 }
 
-AnnotatedCode R2GhidraDecompiler::decompileAt(RVA addr)
+void R2GhidraDecompiler::decompileAt(RVA addr)
 {
+	if(task)
+		return;
+
 	AnnotatedCode code = {};
 
-	auto json = Core()->cmd("pdgj @ " + QString::number(addr));
-	if(json.isEmpty())
-		return code;
+	task = new R2Task ("pdgj @ " + QString::number(addr));
 
-	QJsonParseError jsonError;
-	QJsonDocument doc = QJsonDocument::fromJson(json.toUtf8(), &jsonError);
-	if(jsonError.error != QJsonParseError::NoError)
-	{
-		// Dirty but ENOTIME
-		code.code = json;
-		return code;
-	}
+	connect(task, &R2Task::finished, this, [this]() {
+		AnnotatedCode code = {};
+		QString s;
 
-	auto root = doc.object();
-	code.code = root["code"].toString();
-
-	for(QJsonValueRef annotationValue : root["annotations"].toArray())
-	{
-		QJsonObject annotationObject = annotationValue.toObject();
-		CodeAnnotation annotation = {};
-		annotation.start = (size_t)annotationObject["start"].toVariant().toULongLong();
-		annotation.end = (size_t)annotationObject["end"].toVariant().toULongLong();
-		if(annotationObject["type"].toString() == "offset")
+		QJsonObject json = task->getResultJson().object();
+		delete task;
+		task = nullptr;
+		if(json.isEmpty())
 		{
-			annotation.type = CodeAnnotation::Type::Offset;
-			annotation.offset.offset = annotationObject["offset"].toVariant().toULongLong();
+			code.code = tr("Failed to parse JSON from r2ghidra");
+			emit finished(code);
+			return;
 		}
-		else
-			continue;
-		code.annotations.push_back(annotation);
-	}
 
-	return code;
+		auto root = json;
+		code.code = root["code"].toString();
+
+		for(QJsonValueRef annotationValue : root["annotations"].toArray())
+		{
+			QJsonObject annotationObject = annotationValue.toObject();
+			CodeAnnotation annotation = {};
+			annotation.start = (size_t)annotationObject["start"].toVariant().toULongLong();
+			annotation.end = (size_t)annotationObject["end"].toVariant().toULongLong();
+			if(annotationObject["type"].toString() == "offset")
+			{
+				annotation.type = CodeAnnotation::Type::Offset;
+				annotation.offset.offset = annotationObject["offset"].toVariant().toULongLong();
+			}
+			else
+				continue;
+			code.annotations.push_back(annotation);
+		}
+		emit finished(code);
+	});
+	task->startTask();
+
 }
