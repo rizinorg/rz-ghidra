4dab8835f61d9da0a81b7e4857d35ffc001ea083
Florian Märkl <info@florianmaerkl.de>
2019-12-17 11:05:14 +0100

    Add More Test Code

--- test/bins/dectest.c
+++ test/bins/dectest.c
@@ -1,17 +1,108 @@
 
-int global_var = 42;
+#include <stdint.h>
+#include <stdbool.h>
 
-int get_global_var() {
+#include "types.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+uint32_t global_var = 42;
+
+uint32_t get_global_var() {
 	return global_var;
 }
 
-int global_array[2] = { 1337, 123 };
+uint32_t global_array[2] = { 1337, 123 };
 
-int get_global_array_entry() {
+uint32_t get_global_array_entry() {
 	return global_array[1];
 }
 
-int main() {
+
+void Aeropause(struct Bright *bright, int argc, char **argv);
+
+int main(int argc, char **argv)
+{
+	struct Bright bright;
+	Aeropause(&bright, argc, argv);
 	return 0;
+}
+
+static inline void PrintAmbassador(enum Ambassador ambassador)
+{
+	printf("Ambassador value: ");
+	switch(ambassador)
+	{
+		case AMBASSADOR_PURE:
+			printf("pure");
+			break;
+		case AMBASSADOR_REASON:
+			printf("reason");
+			break;
+		case AMBASSADOR_REVOLUTION:
+			printf("revolution");
+			break;
+		case AMBASSADOR_ECHOES:
+			printf("echoes");
+			break;
+		case AMBASSADOR_WALL:
+			printf("wall");
+			break;
+		case AMBASSADOR_MILLION:
+			printf("million");
+			break;
+		default:
+			break;
+	}
+	printf("\n");
+}
+
+void Aeropause(struct Bright *bright, int argc, char **argv)
+{
+	bright->morning = malloc(sizeof(struct Morning));
+	bright->morning->saved_argc = argc;
+	bright->morning->saved_argv = argv;
+	if(bright->morning->saved_argc < 2)
+	{
+		bright->ambassador = AMBASSADOR_PURE;
+	}
+	else
+	{
+		bright->window.sunlight = bright->morning->saved_argv[1];
+		if(strcmp(bright->window.sunlight, "the  ") == 0)
+			bright->ambassador = AMBASSADOR_REASON;
+		else if(strcmp(bright->window.sunlight, "dark") == 0)
+			bright->ambassador = AMBASSADOR_REVOLUTION;
+		else if(strcmp(bright->window.sunlight, "third") == 0)
+			bright->ambassador = AMBASSADOR_ECHOES;
+		else
+			bright->ambassador = AMBASSADOR_MILLION;
+	}
+	switch(bright->ambassador)
+	{
+		case AMBASSADOR_PURE:
+			printf("pure");
+			break;
+		case AMBASSADOR_REASON:
+			printf("reason");
+			break;
+		case AMBASSADOR_REVOLUTION:
+			printf("revolution");
+			break;
+		case AMBASSADOR_ECHOES:
+			printf("echoes");
+			break;
+		case AMBASSADOR_WALL:
+			printf("wall");
+			break;
+		case AMBASSADOR_MILLION:
+			printf("million");
+			break;
+		default:
+			break;
+	}
+	PrintAmbassador(bright->ambassador);
 }
 
---
+++ test/bins/types.h
@@ -0,0 +1,28 @@
+
+enum Ambassador
+{
+	AMBASSADOR_PURE = 0,
+	AMBASSADOR_REASON = 1,
+	AMBASSADOR_REVOLUTION = 2,
+	AMBASSADOR_ECHOES = 3,
+	AMBASSADOR_WALL = 4,
+	AMBASSADOR_MILLION = 1000000
+};
+
+struct Window
+{
+	const char *sunlight;
+};
+
+struct Morning
+{
+	uint32_t saved_argc;
+	char **saved_argv;
+};
+
+struct Bright
+{
+	struct Morning *morning;
+	struct Window window;
+	enum Ambassador ambassador;
+};
