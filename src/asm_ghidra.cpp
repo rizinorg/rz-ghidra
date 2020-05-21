75e655a6b27d260ba986389f7be859e6201f97d9
FXTi <zjxiang1998@gmail.com>
2020-05-22 03:12:17 +0800

    Fix

--- src/asm_ghidra.cpp
+++ src/asm_ghidra.cpp
@@ -6,10 +6,11 @@
 
 static SleighAsm sasm;
 
-static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len) {
-    sasm.init (a);
+static int disassemble(RAsm *a, RAsmOp *op, const ut8 *buf, int len)
+{
+    sasm.init(a);
 
-    int r = sasm.disassemble (op, a->pc);
+    int r = sasm.disassemble(op, a->pc);
     op->size = r;
     return r;
 }
@@ -21,12 +22,12 @@ RAsmPlugin r_asm_plugin_ghidra = {
     /* .version = */ nullptr,
     /* .cpus = */ nullptr,
     /* .desc = */ "SLEIGH Disassembler from Ghidra",
-	/* .license = */ "GPL3",
+    /* .license = */ "GPL3",
     /* .user = */ nullptr,
     /* .bits = */ 0,
     /* .endian = */ 0,
     /*.init = */ nullptr,
-	/*.fini = */ nullptr,
+    /*.fini = */ nullptr,
     /* .disassemble = */ &disassemble,
     /* .assemble = */ nullptr,
     /* .modify */ nullptr,
@@ -38,13 +39,14 @@ RAsmPlugin r_asm_plugin_ghidra = {
 #ifdef __cplusplus
 extern "C"
 #endif
-R_API RLibStruct radare_plugin = {
+    R_API RLibStruct radare_plugin = {
 	/* .type = */ R_LIB_TYPE_ASM,
 	/* .data = */ &r_asm_plugin_ghidra,
 	/* .version = */ R2_VERSION,
 	/* .free = */ nullptr
 #if R2_VERSION_MAJOR >= 4 && R2_VERSION_MINOR >= 2
-	, "r2ghidra-dec"
+	,
+	"r2ghidra-dec"
 #endif
-};
+    };
 #endif
