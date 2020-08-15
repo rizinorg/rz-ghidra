199ff3b119bb9474c782989e37d6e0b214c7507c
yossizap <yossizap@gmail.com>
2020-08-15 09:17:11 +0000

    Remove Cutter plugin build from build_msys temporarily

--- scripts/msys_build.sh
+++ scripts/msys_build.sh
@@ -1,11 +1,11 @@
+#!/bin/bash
+
 mkdir -p build && cd build
 cmake ../ -G Ninja \
     -DCMAKE_C_COMPILER=cl \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_PREFIX_PATH="$INSTALL_PREFIX;$INSTALL_PREFIX/include/libr;$INSTALL_PREFIX/include/libr/sdb;$QT64PATH" \
+    -DCMAKE_PREFIX_PATH="$INSTALL_PREFIX;$INSTALL_PREFIX/include/libr;$INSTALL_PREFIX/include/libr/sdb" \
     -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
-    -DBUILD_CUTTER_PLUGIN=ON \
-    -DCUTTER_SOURCE_DIR="$CUTTER_PATH" \
     -DBUILD_SLEIGH_PLUGIN=ON
 ninja
 ninja install
