f61261070fc0cbb2129dd9ee7f99827a77193d58
yossizap <yossizap@users.noreply.github.com>
2020-08-15 13:19:33 +0300

    Remove sleigh plugin build from windows for now

--- scripts/msys_build.sh
+++ scripts/msys_build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-mkdir -p build && cd build
+mkdir -p build && cd build || exit 1
 cmake ../ -G Ninja \
     -DCMAKE_C_COMPILER=cl \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH="$INSTALL_PREFIX;$INSTALL_PREFIX/include/libr;$INSTALL_PREFIX/include/libr/sdb" \
-    -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
-    -DBUILD_SLEIGH_PLUGIN=ON
-ninja
-ninja install
+    -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" || exit 1
+ninja || exit 1
+ninja install || exit 1
