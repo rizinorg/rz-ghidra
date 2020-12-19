352b6fe1f7cd268c0d9a809b560d298dc19aa080
yossizap <yossizap@users.noreply.github.com>
2020-12-19 12:08:51 +0200

    Update rizin paths

--- scripts/msys_build.sh
+++ scripts/msys_build.sh
@@ -4,7 +4,7 @@ mkdir -p build && cd build || exit 1
 cmake ../ -G Ninja \
     -DCMAKE_C_COMPILER=cl \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_PREFIX_PATH="$INSTALL_PREFIX;$INSTALL_PREFIX/include/libr;$INSTALL_PREFIX/include/libr/sdb" \
+    -DCMAKE_PREFIX_PATH="$INSTALL_PREFIX;$INSTALL_PREFIX/include/librz;$INSTALL_PREFIX/include/librz/sdb" \
     -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" || exit 1
 ninja || exit 1
 ninja install || exit 1
