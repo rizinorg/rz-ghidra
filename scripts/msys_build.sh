a73c801e7869d49d13b768631eae39d02d07e25f
yossizap <yossizap@users.noreply.github.com>
2020-12-19 12:12:38 +0200

    Disable SLEIGH

--- scripts/msys_build.sh
+++ scripts/msys_build.sh
@@ -5,6 +5,7 @@ cmake ../ -G Ninja \
     -DCMAKE_C_COMPILER=cl \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH="$INSTALL_PREFIX;$INSTALL_PREFIX/include/librz;$INSTALL_PREFIX/include/librz/sdb" \
-    -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" || exit 1
+    -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
+    -DBUILD_SLEIGH_PLUGIN=OFF || exit 1
 ninja || exit 1
 ninja install || exit 1
