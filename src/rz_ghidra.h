c6c8348a84df056eef3611be4347b9c99a6b9449
Anton Kochkov <anton.kochkov@gmail.com>
2021-05-08 16:47:02 +0800

    Use extern "C" for exported symbols

--- src/rz_ghidra.h
+++ src/rz_ghidra.h
@@ -4,6 +4,14 @@
 #include <rz_util/rz_annotated_code.h>
 #include <rz_core.h>
 
+#if __cplusplus
+extern "C" {
+#endif
+
 RZ_API RzAnnotatedCode *rz_ghidra_decompile_annotated_code(RzCore *core, ut64 addr);
+
+#if __cplusplus
+}
+#endif
 
 #endif //RZ_GHIDRA_H
